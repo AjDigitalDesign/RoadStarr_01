1acb175e69a88f5c4513afe49d45cbdd9537ce73
ajdigitaldesign <ajardiahjr@gmail.com>
2021-05-23 09:51:35 -0400

    third commit

--- components/services.js
+++ components/services.js
@@ -9,34 +9,28 @@ export default function Services(){
                     <h2 class="text-center mt-0">At Your Service</h2>
                     <hr class="divider" />
                     <div class="row gx-4 gx-lg-5">
-                        <div class="col-lg-3 col-md-6 text-center">
+                        <div class="col-lg-4 col-md-4 text-center">
                             <div class="mt-5">
-                                <div class="mb-2"><i class="bi-gem fs-1 text-primary"></i></div>
-                                <h3 class="h4 mb-2">Sturdy Themes</h3>
+                                <div class="mb-2"><i class="fas fa-car"></i></div>
+                                <h3 class="h4 mb-2">Tow service</h3>
                                 <p class="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-6 text-center">
+                        <div class="col-lg-4 col-md-4 text-center">
                             <div class="mt-5">
-                                <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>
-                                <h3 class="h4 mb-2">Up to Date</h3>
+                                <div class="mb-2"><i class="fas fa-car-battery"></i></div>
+                                <h3 class="h4 mb-2">Jump Start or Battery Test</h3>
                                 <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-6 text-center">
+                        <div class="col-lg-4 col-md-4 text-center">
                             <div class="mt-5">
-                                <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div>
-                                <h3 class="h4 mb-2">Ready to Publish</h3>
+                                <div class="mb-2"><i class="fas fa-car"></i></div>
+                                <h3 class="h4 mb-2">Tire Change </h3>
                                 <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-6 text-center">
-                            <div class="mt-5">
-                                <div class="mb-2"><i class="bi-heart fs-1 text-primary"></i></div>
-                                <h3 class="h4 mb-2">Made with Love</h3>
-                                <p class="text-muted mb-0">Is it really open source if it's not made with love?</p>
-                            </div>
-                        </div>
+                    
                     </div>
                 </div>
             </section>
