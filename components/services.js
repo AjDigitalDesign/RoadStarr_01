15495a2c29212cf828880e806586b9ab40d28316
ajdigitaldesign <ajardiahjr@gmail.com>
2021-05-23 10:09:14 -0400

    third commit

--- components/services.js
+++ components/services.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 
+
 export default function Services(){
     return(
         <>
@@ -19,7 +20,7 @@ export default function Services(){
                         <div class="col-lg-4 col-md-4 text-center">
                             <div class="mt-5">
                                 <div class="mb-2"><i class="fas fa-car-battery"></i></div>
-                                <h3 class="h4 mb-2">Jump Start or Battery Test</h3>
+                                <h3 class="h4 mb-2">Jump Start or Battery Testing</h3>
                                 <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
                             </div>
                         </div>
